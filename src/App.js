3f98044d75a4af785b940e99281ed430338817a0
Petras-aleksandar988 <104795401+Petras-aleksandar988@users.noreply.github.com>
2023-03-18 17:55:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import MovieCard from "./MovieCard";
 
 const api_url = "http://omdbapi.com?apikey=eb01f497";
 
-const movie1 = {
-  Title: "Italian Spiderman",
-  Year: "2007",
-  imdbID: "tt2705436",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
